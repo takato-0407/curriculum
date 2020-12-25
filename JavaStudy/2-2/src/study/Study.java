51324cba40d9bffcce14ff4ef7f70d655f28ef75
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2020-12-25 14:15:24 +0900

    修正完了

--- JavaStudy/2-2/src/study/Study.java
+++ JavaStudy/2-2/src/study/Study.java
@@ -13,13 +13,13 @@ class Account {
     // コンストラクタ
     public Account(String userName, int id, String password) {
         this.userName = userName;
-        this.id = 1;
+        this.id = id;
         this.password = password;
     }
     
     protected void printAccountInfo() {
     	System.out.println(userName);
-    	System.out.println(1);
+    	System.out.println(id);
     	System.out.println(password);
     }
     // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
