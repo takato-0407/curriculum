696c174fa4fc16b7cd3e9da42d7554eb29f1b5ab
takato-0407 <70625268+takato-0407@users.noreply.github.com>
2021-03-09 00:25:05 +0900

    2-3完了

---
+++ JavaStudy/2-3/src/study/Task.java
@@ -0,0 +1,23 @@
+package study;
+
+// ① TaskクラスにCalculatorクラスを継承させなさい。
+    public class Task extends Calculator{
+    /**
+     * タスクの実行
+     */
+    	private int a = 10;
+    	private int b = 20;
+    	private int c = 30;
+  
+    public void doTask(int a, int b, int c) {
+        this.a = 10;
+        this.b = 20;
+        this.c = 30;
+     }
+    
+    public void doTask() {
+    	System.out.println("plusメゾットの引数が一つの場合" + ";" + plus(a));
+    	System.out.println("plusメゾットの引数が一つの場合" + ";" + plus(a, b));
+    	System.out.println("plusメゾットの引数が一つの場合" + ";" + plus(a, b, c));
+    }
+}
